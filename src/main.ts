b89ff19cd53cf106ff6c1e289048aee315399a48
cleancodelover <verkyavpeter@gmail.com>
2025-02-22 11:36:58 +0100

    validate core origin

--- src/main.ts
+++ src/main.ts
@@ -13,7 +13,7 @@ async function bootstrap() {
     type: VersioningType.URI, // or VersioningType.HEADER / MEDIA_TYPE
   });
   app.enableCors({
-    origin: 'http://localhost:3001', // Allow specific origin
+    origin: 'https://nft-minting-ui-tfxg.onrender.com', // Allow specific origin
     credentials: true, // Allow cookies, authentication headers
   });
   // Use Helmet to secure headers
